dd96b7babf0c59d37df04273bc53b5bb38cef476
kirurr <timon09df@gmail.com>
2025-04-22 14:48:02 +0300

    add unauthorized global exception

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,23 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { INestApplication } from '@nestjs/common';
 
 function initSwagger(app: INestApplication<any>) {
-
   const config = new DocumentBuilder()
     .setTitle('Nest Melody Match')
     .setDescription('Nest Melody Match API')
     .setVersion('1.0')
-    .addGlobalResponse({
-      status: 500,
-      description: 'Internal server error',
-    })
+    .addGlobalResponse(
+      {
+        status: 500,
+        description: 'Internal server error',
+      },
+      { status: 401, description: 'Unauthorized' },
+    )
     .addBearerAuth()
     .addOAuth2({
       type: 'oauth2',
       flows: {
         authorizationCode: {
-          authorizationUrl:
-            'https://accounts.google.com/o/oauth2/auth',
+          authorizationUrl: 'https://accounts.google.com/o/oauth2/auth',
           tokenUrl: 'https://oauth2.googleapis.com/token',
           scopes: {
             profile: 'Profile scope',
@@ -40,8 +41,8 @@ function initSwagger(app: INestApplication<any>) {
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         scopes: ['email', 'profile'],
-      }
-    }
+      },
+    },
   });
 }
 
